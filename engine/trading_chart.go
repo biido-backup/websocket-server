16c4a09286b344a8ba0f8f65ea7b9ef6370006cd
Fadhlan Hidayat <mail.fadhlanh@gmail.com>
2019-02-24 18:56:19 +0700

    uncomment set rate btc-idr only

--- engine/trading_chart.go
+++ engine/trading_chart.go
@@ -15,7 +15,7 @@ func ProcessTradingChart(tradingRateList []daos.Rate) {
 	daos.CreateTradingChart()
 	quantity := viper.GetInt64("tradingchart.quantity")
 
-	tradingRateList = []daos.Rate{{"BTC", "IDR"}}
+	//tradingRateList = []daos.Rate{{"BTC", "IDR"}}
 	unitOfTimeList := []string{"1M", "5M", "15M", "30M", "1H", "6H", "12H", "1D", "1W", "1MO"}
 	for _, tradingRate := range tradingRateList {
 		daos.InitRateTradingChart(tradingRate.StringDash())
