3515d6925997242d78bc0e87b6218649e885fb47
agung <agung@biido.id>
2019-02-26 05:19:30 +0700

    open logging

--- util/zeromq/zeromq.go
+++ util/zeromq/zeromq.go
@@ -176,7 +176,7 @@ func ListenLast24h(publisher string, topic string, zmqKey string, clients *daos.
 			log.Error("error when marshal last24h", err)
 		}
 
-		//log.Println(trdLast24h)
+		log.Println(trdLast24h)
 		cache.SetCacheByTopicAndType(topic, trdconst.LAST24H, trdLast24h)
 
 		websocket.BroadcastMessage(topic, string(trdLast24hJson))
@@ -231,7 +231,7 @@ func ListenTradingHistory(publisher string, topic string, zmqKey string, clients
 			log.Error("error when marshal listHistory", err)
 		}
 
-		//log.Println(trdListHistory)
+		log.Println(trdListHistory)
 		cache.SetCacheByTopicAndType(topic, trdconst.TRADINGHISTORY, trdListHistory)
 
 		websocket.BroadcastMessage(topic, string(trdListHistoryJson))
@@ -286,7 +286,7 @@ func ListenOpenOrder(publisher string, topic string, zmqKey string, clients *dao
 		cache.SetCacheByTopicAndType(topic, trdconst.OPENORDER, trdListOpenOrder)
 		username := listOpenOrder.Username
 		//log.Println("username : ",username)
-		//log.Println(trdListOpenOrder)
+		log.Println(trdListOpenOrder)
 		websocket.SendMessageToUser(topic, username, string(trdListOpenOrderJson))
 
 	}
@@ -339,7 +339,7 @@ func ListenOrderHistory(publisher string, topic string, zmqKey string, clients *
 
 		username := listOrderHistory.Username
 		//log.Println("username : ",username)
-		//log.Println(trdListOrderHistory)
+		log.Println(trdListOrderHistory)
 		websocket.SendMessageToUser(topic, username, string(trdListOrderHistoryJson))
 
 	}
